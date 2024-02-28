28e6f81a8dc509c10bcc41e77bb8bd25d4da2ba9
Leileisme <145089167+Leileisme@users.noreply.github.com>
2024-02-28 11:07:59 +0000

    Deploying to gh-pages from @ Leileisme/ClubWeb-front@037c97f1eb92a20c2129f7ccd09fb73ba6fc98a4 🚀

---
+++ assets/BlankLine-762864e2.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper-c27b6911.js";import{V as r}from"./VDivider-1072cf3f.js";import{V as e}from"./VCol-dd1ffe34.js";import{D as c,E as t,F as a,v as s}from"./index-ad800216.js";const f={};function i(n,_){return c(),t(e,{cols:"12"},{default:a(()=>[s(r,{color:"#fff",class:"border-opacity-50"})]),_:1})}const V=o(f,[["render",i]]);export{V as B};
